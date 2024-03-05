1382b2480c8f57077782d5b956be1bd372a7a64c
Akos Bognar <75569513+bognarit@users.noreply.github.com>
2024-03-05 23:51:43 +0100

    hp-nonfood_05.03

---
+++ script/non-food.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const products = document.getElementById('non_food_products')
+
+fetch('./non-foods.json')
+    .then(results => results.json())
+    .then(data => {
+        data.forEach(post => {
+            products.insertAdjacentHTML("beforeend",
+            `<div>
+                <article class="article">
+                    <img src="${post.image}" width="300" height="300">
+                    <h2 class="article_title">${post.title}</h2>
+                    <div class="article_price">${post.price} €</div>
+                </article>
+            </div>
+            `
+            )
+        })
+    });
