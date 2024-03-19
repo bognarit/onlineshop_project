c0eb2285ff2588ba677c6c10dead2fe07823aa9e
Akos Bognar <75569513+bognarit@users.noreply.github.com>
2024-03-19 22:42:19 +0100

    new_home_page_19.03

---
+++ script/non-food-script.js
@@ -0,0 +1,95 @@
+'use strict';
+
+export const nonFoods = [
+    {
+        id: 1,
+        title: 'Stuhl',
+        category: 'Möbel',
+        price: 16.90,
+        image: './img/non-food/non-food 01.jpg'
+    },
+    {
+        id: 2,
+        title: 'Tisch',
+        category: 'Möbel',
+        price: 129.49,
+        image: './img/non-food/non-food 02.jpg'
+    },
+    {
+        id: 3,
+        title: 'Bett',
+        category: 'Möbel',
+        price: 229.99,
+        image: './img/non-food/non-food 03.jpg'
+    },
+    {
+        id: 4,
+        title: 'Schreibtischleuchte',
+        category: 'Elektronische Geräte',
+        price: 19.99,
+        image: './img/non-food/non-food 04.jpg'
+    },
+    {
+        id: 5,
+        title: 'Radio',
+        category: 'Elektronische Geräte',
+        price: 49.99,
+        image: './img/non-food/non-food 05.jpg'
+    },
+    {
+        id: 6,
+        title: 'Fernseher',
+        category: 'Elektronische Geräte',
+        price: 59.99,
+        image: './img/non-food/non-food 06.jpg'
+    },
+    {
+        id: 7,
+        title: 'Telefon',
+        category: 'Elektronische Geräte',
+        price: 27.99,
+        image: './img/non-food/non-food 07.jpg'
+    },
+    {
+        id: 8,
+        title: 'Uhr 1',
+        category: 'Uhren',
+        price: 24.99,
+        image: './img/non-food/non-food 08.jpg'
+    },
+    {
+        id: 9,
+        title: 'Uhr 2',
+        category: 'Uhren',
+        price: 49.99,
+        image: './img/non-food/non-food 09.jpg'
+    },
+    {
+        id: 10,
+        title: 'Flüssigseife',
+        category: 'Haushalt',
+        price: 2.25,
+        image: './img/non-food/non-food 10.jpg'
+    },
+    {
+        id: 11,
+        title: 'Seife',
+        category: 'Haushalt',
+        price: 0.99,
+        image: './img/non-food/non-food 11.jpg'
+    },
+    {
+        id: 12,
+        title: 'Waschmittel',
+        category: 'Haushalt',
+        price: 14.99,
+        image: './img/non-food/non-food 12.jpg'
+    },
+    {
+        id: 13,
+        title: 'Zahnbürste',
+        category: 'Drogerie',
+        price: 1.99,
+        image: './img/non-food/non-food 13.jpg'
+    }
+];
--- script/script.js
+++ script/script.js
@@ -1,22 +1,124 @@
 'use strict';
 
-const modal = document.querySelector('.modal');
-const overlay = document.querySelector('.overlay');
-const btnsCloseModal = document.querySelector('.btn--close-modal');
-const btnsOpenModal = document.querySelector('.btn--show-modal');
+import { foods } from "../script/food-script.js";
+import { nonFoods } from "../script/non-food-script.js";
 
-const openModal = function (e) {
-    e.preventDefault();
-    modal.classList.remove('hidden');
-    overlay.classList.remove('hidden');
+const searchForm = document.querySelector('.search-form');
+const shoppingCart = document.querySelector('.shopping-cart');
+const loginForm = document.querySelector('.login-form');
+const navbar = document.querySelector('.navbar');
+const foodSection = document.querySelector('.food-slider');
+const nonFoodSection = document.querySelector('.non-food-slider');
+
+document.querySelector('#search-btn').onclick = () => {
+    searchForm.classList.toggle('active');
+    shoppingCart.classList.remove('active');
+    loginForm.classList.remove('active');
+    navbar.classList.remove('active');
 };
 
-const closeModal = function (e) {
-    e.preventDefault();
-    modal.classList.add('hidden');
-    overlay.classList.add('hidden');
+document.querySelector('#cart-btn').onclick = () => {
+    shoppingCart.classList.toggle('active');
+    searchForm.classList.remove('active');
+    loginForm.classList.remove('active');
+    navbar.classList.remove('active');
 };
 
-btnsOpenModal.addEventListener('click', openModal);
-btnsCloseModal.addEventListener('click', closeModal);
-overlay.addEventListener('click', closeModal);
+document.querySelector('#login-btn').onclick = () => {
+    loginForm.classList.toggle('active');
+    searchForm.classList.remove('active');
+    shoppingCart.classList.remove('active');
+    navbar.classList.remove('active');
+};
+
+document.querySelector('#menu-btn').onclick = () => {
+    navbar.classList.toggle('active');
+    shoppingCart.classList.remove('active');
+    searchForm.classList.remove('active');
+    loginForm.classList.remove('active');
+}
+
+window.onscroll = () => {
+    searchForm.classList.remove('active');
+    shoppingCart.classList.toggle('active');
+    loginForm.classList.remove('active');
+    navbar.classList.remove('active');
+}
+
+const foodProducts = function (food) {
+    foods.forEach(food => {
+        foodSection.insertAdjacentHTML("beforeend",
+        `<div class="swiper-wrapper">
+            <div class="swiper-slide box">
+                <img src="${food.image}" width="300" height="300">
+                <h2 class="title">${food.title}</h2>
+                <div class="price">${food.price} €</div>
+                <a href="#" class="btn">In den Warenkorb</a>
+            </div>
+        </div>
+        `
+        )
+    })
+};
+
+foodProducts(foods);
+
+const nonFoodProducts = function (nonfood) {
+    nonFoods.forEach(nonfood => {
+        nonFoodSection.insertAdjacentHTML("beforeend",
+        `<div class="swiper-wrapper">
+            <div class="swiper-slide box">
+                <img src="${nonfood.image}" width="300" height="300">
+                <h2 class="title">${nonfood.title}</h2>
+                <div class="price">${nonfood.price} €</div>
+                <a href="#" class="btn">In den Warenkorb</a>
+            </div>
+        </div>
+        `
+        )
+    })
+};
+
+nonFoodProducts(nonFoods);
+
+var swiper = new Swiper(".products-cat-slider", {
+  loop: true,
+  spaceBetween: 20,
+  autoplay: {
+      delay: 7500,
+      disableOnInteraction: false,
+  },
+  centeredSlides: true,
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1020: {
+      slidesPerView: 3,
+    },
+  },
+});
+
+// var swiper = new Swiper(".food-slider", {
+//     loop: true,
+//     spaceBetween: 20,
+//     autoplay: {
+//         delay: 7500,
+//         disableOnInteraction: false,
+//     },
+//     centeredSlides: true,
+//     breakpoints: {
+//         0: {
+//         slidesPerView: 1,
+//         },
+//         768: {
+//         slidesPerView: 2,
+//         },
+//         1020: {
+//         slidesPerView: 3,
+//         },
+//     },
+//     });
